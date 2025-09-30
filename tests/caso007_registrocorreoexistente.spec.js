f276f05b0f43769100e3390a8b100e0f36f757fa
MayraCayo <142956333+MayraCayo@users.noreply.github.com>
2025-09-30 12:14:18 -0400

    Update caso007_registrocorreoexistente.spec.js

--- tests/caso007_registrocorreoexistente.spec.js
+++ tests/caso007_registrocorreoexistente.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost/opencart/upload/');
+  await page.goto('http://localhost:8080/');
   await page.getByRole('link', { name: ' My Account ' }).click();
   await page.getByRole('link', { name: 'Register' }).click();
   await page.getByRole('textbox', { name: '* First Name' }).click();
